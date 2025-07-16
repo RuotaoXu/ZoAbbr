ed8a26fd66c120fa28ddb536e2643d610a6ecaca
RuotaoXu <rtxu@superobots.com>
2025-07-16 16:32:26 +0800

    update

--- src/hooks.ts
+++ src/hooks.ts
@@ -23,14 +23,6 @@ async function onStartup() {
 
   BasicExampleFactory.registerNotifier();
 
-  // KeyExampleFactory.registerShortcuts();
-
-  // await UIExampleFactory.registerExtraColumn();
-
-  // await UIExampleFactory.registerExtraColumnWithCustomCell();
-
-  // UIExampleFactory.registerItemPaneCustomInfoRow();
-
   UIExampleFactory.registerItemPaneSection();
 
   // UIExampleFactory.registerReaderItemPaneSection();
@@ -43,52 +35,14 @@ async function onStartup() {
 async function onMainWindowLoad(win: _ZoteroTypes.MainWindow): Promise<void> {
   // Create ztoolkit for every window
   addon.data.ztoolkit = createZToolkit();
-
   // @ts-ignore This is a moz feature
   win.MozXULElement.insertFTLIfNeeded(
     `${addon.data.config.addonRef}-mainWindow.ftl`,
   );
-
-  // const popupWin = new ztoolkit.ProgressWindow(addon.data.config.addonName, {
-  //   closeOnClick: true,
-  //   closeTime: -1,
-  // })
-  //   .createLine({
-  //     text: getString("startup-begin"),
-  //     type: "default",
-  //     progress: 0,
-  //   })
-  //   .show();
+  await Zotero.Promise.delay(1000);
+  myHelper.registerRightClickMenuItem();
 
   await Zotero.Promise.delay(1000);
-  // popupWin.changeLine({
-  //   progress: 30,
-  //   text: `[30%] ${getString("startup-begin")}`,
-  // });
-
-  // UIExampleFactory.registerStyleSheet(win);
-
-  // UIExampleFactory.registerRightClickMenuItem();
-
-  // UIExampleFactory.registerRightClickMenuPopup(win);
-
-  // UIExampleFactory.registerWindowMenuWithSeparator();
-
-  // PromptExampleFactory.registerNormalCommandExample();
-
-  // PromptExampleFactory.registerAnonymousCommandExample(win);
-
-  // PromptExampleFactory.registerConditionalCommandExample();
-
-  await Zotero.Promise.delay(1000);
-
-  // popupWin.changeLine({
-  //   progress: 100,
-  //   text: `[100%] ${getString("startup-finish")}`,
-  // });
-  // popupWin.startCloseTimer(5000);
-
-  // addon.hooks.onDialogEvents("dialogExample");
 }
 
 async function onMainWindowUnload(win: Window): Promise<void> {
@@ -109,25 +63,6 @@ function onShutdown(): void {
  * This function is just an example of dispatcher for Notify events.
  * Any operations should be placed in a function to keep this funcion clear.
  */
-async function onNotify_old(
-  event: string,
-  type: string,
-  ids: Array<string | number>,
-  extraData: { [key: string]: any },
-) {
-  // You can add your code to the corresponding notify type
-  ztoolkit.log("notify", event, type, ids, extraData);
-  if (
-    event == "select" &&
-    type == "tab" &&
-    extraData[ids[0]].type == "reader"
-  ) {
-    BasicExampleFactory.exampleNotifierCallback();
-  } else {
-    return;
-  }
-}
-
 async function onNotify(
 
   event: string,
@@ -162,57 +97,6 @@ async function onNotify(
   }
 }
 
-/**
- * This function is just an example of dispatcher for Preference UI events.
- * Any operations should be placed in a function to keep this funcion clear.
- * @param type event type
- * @param data event data
- */
-async function onPrefsEvent(type: string, data: { [key: string]: any }) {
-  switch (type) {
-    case "load":
-      registerPrefsScripts(data.window);
-      break;
-    default:
-      return;
-  }
-}
-
-function onShortcuts(type: string) {
-  switch (type) {
-    case "larger":
-      KeyExampleFactory.exampleShortcutLargerCallback();
-      break;
-    case "smaller":
-      KeyExampleFactory.exampleShortcutSmallerCallback();
-      break;
-    default:
-      break;
-  }
-}
-
-function onDialogEvents(type: string) {
-  switch (type) {
-    case "dialogExample":
-      HelperExampleFactory.dialogExample();
-      break;
-    case "clipboardExample":
-      HelperExampleFactory.clipboardExample();
-      break;
-    case "filePickerExample":
-      HelperExampleFactory.filePickerExample();
-      break;
-    case "progressWindowExample":
-      HelperExampleFactory.progressWindowExample();
-      break;
-    case "vtableExample":
-      HelperExampleFactory.vtableExample();
-      break;
-    default:
-      break;
-  }
-}
-
 // Add your hooks here. For element click, etc.
 // Keep in mind hooks only do dispatch. Don't add code that does real jobs in hooks.
 // Otherwise the code would be hard to read and maintain.
@@ -223,7 +107,4 @@ export default {
   onMainWindowLoad,
   onMainWindowUnload,
   onNotify,
-  onPrefsEvent,
-  onShortcuts,
-  onDialogEvents,
 };
--- src/modules/myfunc.ts
+++ src/modules/myfunc.ts
@@ -1,4 +1,59 @@
-function getSeries(name: string) {
+import { getLocaleID, getString } from "../utils/locale";
+const series_mapping: [string, RegExp][] = [
+  ["3DV", /.*Int.*Conf.*3D Vision.*/i],
+  ["3Dor", /.*3D.*object.*retrieval.*/i],
+  ["Access", /.*Access.*/i],
+  ["AAAI", /.*AAAI.*Conf.*Artificial.*Intell.*/i],
+  ["ACHA", /.*App.*Comput.*Harmon.*Anal.*/i],
+  ["arXiv", /.*arXiv.*/i],
+  ["BMVC", /.*British.*Machine.*Vis.*Conf.*/i],
+  ["CVPR", /.*Comput.*Vis.*Pattern.*Recognition.*/i],
+  ["CGF", /.*Computer.*graphics.*forum.*/i],
+  ["ECCV", /.*Eur.*Conf.*Comput.*Vision.*/i],
+  ["ICASSP", /.*Int.*Conf.*Acoustics.*Speech.*Signal.*/i],
+  ["ICCV", /.*Int.*Conf.*Comput.*Vision.*/i],
+  ["ICIP", /.*Int.*Conf.*Image.*Process.*/i],
+  ["ICLR", /.*Int.*Conf.*Learn.*Representation.*/i],
+  ["IROS", /.*Int.*Conf.*Robot.*system.*/i],
+  ["ICRA", /.*Int.*Conf.*Robot.*Automation.*/i],
+  ["ICME", /.*Int.*Conf.*Multimedia.*Expo.*/i],
+  ["ICML", /.*Int.*Conf.*Mach.*Learn.*/i],
+  ["IJCAI", /.*Int.*Joint.*Conf.*Artificial.*Intell.*/i],
+  ["IJCNN", /.*Int.*Joint.*Conf.*Neural.*Network.*/i],
+  ["IJCV", /.*Int.*J.*Comput.*Vision.*/i],
+  ["IS", /.*Inform.*Sci.*/i],
+  ["JMIV", /.*J.*Math.*Imaging.*Vision.*/i],
+  ["JSC", /.*J.*Scientific.*Computing.*/i],
+  ["MM", /.*Int.*Conf.*Multimedia.*/i],
+  ["MMS", /.*Multiscale.*Model.*Simul.*/i],
+  ["Neurocomputing", /.*Neurocomputing.*/i],
+  ["NIPS", /.*Advances.*Neural.*Inform.*Process.*Syst.*/i],
+  ["PAMI", /.*Trans.*Pattern.*Anal.*Mach.*Intell.*/i],
+  ["PR", /.*Pattern.*Recognition.*/i],
+  ["RAL", /.*Robotics.*Automation.*Letters.*/i],
+  ["SIIMS", /.*SIAM.*Journal.*Imaging.*Sci.*/i],
+  ["SPL", /.*Signal.*Process.*Letters.*/i],
+  ["TCSVT", /.*Trans.*Circuits.*Syst.*Video.*Technology.*/i],
+  ["TCYB", /.*Trans.*Cybernetics.*/i],
+  ["TGRS", /.*Trans.*Geosci.*Remote.*Sens.*/i],
+  ["TIFS", /.*Inform.*Forensics.*Security.*/i],
+  ["TIP", /.*Trans.*Image.*Process.*/i],
+  ["TMM", /.*Trans.*Multimedia.*/i],
+  ["TNNLS", /.*Trans.*Neural.*Netw.*Learn.*Syst.*/i],
+  ["TVCG", /.*Trans.*Vis.*Comput.*Graph.*/i],
+  ["TOG", /.*Trans.*Graphics.*/i],
+  ["TSP", /.*Trans.*Signal.*Process.*/i],
+  ["WACV", /.*Winter.*Conf.*App.*Comput.*Vision.*/i],
+  ["NN", /.*Neural.*Networks.*/i],
+  ["SPIC", /.*Signal.*Process.*Image.*Communication.*/i],
+  ["SP", /.*Signal.*Process.*/i],
+  ["JMLR", /Journal of Machine Learning Research/i],
+  ["MICCAI", /Medical Image Computing and Computer.*Assisted Intervention/i],
+  ["ACCV", /Asian Conference on Computer Vision/i],
+  ["CoRL", /Conference on Robot Learning/i]
+];
+
+function getSeries_old(name: string) {
   let series = "";
   if (name.match("3DV")) {
     series = "3DV";
@@ -102,7 +157,7 @@ function getSeries(name: string) {
     series = "arXiv";
   } else if (name.match(/.*British.*Machine.*Vis.*Conf.*/i)) {
     series = "BMVC";
-  } else if (name.match(/.*Conf.*Comput.*Vis.*Pattern.*Recognition.*/i)) {
+  } else if (name.match(/.*Comput.*Vis.*Pattern.*Recognition.*/i)) {
     series = "CVPR";
   } else if (name.match(/.*Computer.*graphics.*forum.*/i)) {
     series = "CGF";
@@ -198,34 +253,61 @@ function getSeries(name: string) {
   //   }
   return series;
 }
-export class myHelper {
-  static update(item: any) {
-    let name = "";
-    if (item.getField("conferenceName")) {
-      name = item.getField("conferenceName");
-    } else if (item.getField("publicationTitle")) {
-      name = item.getField("publicationTitle");
-    } else if (item.getField("proceedingsTitle")) {
-      name = item.getField("proceedingsTitle");
-    } else if (item.getField("repository")) {
-      name = item.getField("repository");
-    }
-    let series = "";
-    if (item.getField("series")) {
-      series = item.getField("series");
-    } else {
-      series = getSeries(name);
-      item.setField("series", series);
-    }
-    item.setField("seriesTitle", item.getField("year").slice(-2));
-    item.saveTx();
-    // ztoolkit.getGlobal("alert")(series);
-  }
-  static updateSelected() {
 
-    const items = Zotero.getActiveZoteroPane().getSelectedItems();
-    for (let i = 0; i < items.length; i++) {
-      myHelper.update(items[i]);
+function getSeries(name: string) {
+  let series = "";
+  for (const [seriesName, pattern] of series_mapping){
+    if (name.match(seriesName)) {
+      series = seriesName;
+      break;
+    } else if (name.match(pattern)){
+      series = seriesName;
+      break;
     }
   }
+  return series;
 }
+  export class myHelper {
+    static update(item: any) {
+      let name = "";
+      if (item.getField("conferenceName")) {
+        name = item.getField("conferenceName");
+      } else if (item.getField("publicationTitle")) {
+        name = item.getField("publicationTitle");
+      } else if (item.getField("proceedingsTitle")) {
+        name = item.getField("proceedingsTitle");
+      } else if (item.getField("repository")) {
+        name = item.getField("repository");
+      }
+      // ztoolkit.getGlobal("alert")(name);
+      let series = "";
+      if (item.getField("series")) {
+        series = item.getField("series");
+        item.saveTx();
+      } else {
+        series = getSeries(name);
+        item.setField("series", series);
+        item.saveTx();
+      }
+      item.setField("seriesTitle", item.getField("year").slice(-2));
+      item.saveTx();
+      // ztoolkit.getGlobal("alert")(series);
+    }
+    static updateSelected() {
+      const items = Zotero.getActiveZoteroPane().getSelectedItems();
+      for (let i = 0; i < items.length; i++) {
+        myHelper.update(items[i]);
+      }
+    }
+    static registerRightClickMenuItem() {
+      const menuIcon = `chrome://${addon.data.config.addonRef}/content/icons/favicon@0.5x.png`;
+      // item menuitem with icon
+      ztoolkit.Menu.register("item", {
+        tag: "menuitem",
+        id: "zotero-itemmenu-addontemplate-test",
+        label: getString("更新缩写"),
+        commandListener: (ev) => myHelper.updateSelected(),
+        icon: menuIcon,
+      });
+    }
+  }
